bdebcf7303ca4962f36c615a194a2aedea4ae5ec
David Hörl <hoerldavid@gmail.com>
2018-04-12 16:35:05 +0200

    do not listen on localhost, require netifaces

--- src/autodetect-client-test.py
+++ src/autodetect-client-test.py
@@ -1,5 +1,5 @@
 from xmlrpc.client import ServerProxy
 
-with ServerProxy("http://localhost:8000/") as proxy:
-    print(proxy.detect_bbox('acs'))
+with ServerProxy("gpu-1:8000/") as proxy:
+    print(proxy.detect_bbox('/scratch/bunk/unet/images/test/000.tif'))
 
--- src/autodetect.py
+++ src/autodetect.py
@@ -4,6 +4,12 @@ import argparse
 from calmutils.segmentation import Tools
 from calmutils.imageio import read_bf
 
+import netifaces as ni
+
+def get_ip(interface='eth0'):
+    ni.ifaddresses(interface)
+    ip = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
+    return ip
 
 class Worker:
     def __init__(self, unet_conf_dir):
@@ -21,10 +27,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('unet_dir', help='directory of U-net project')
     parser.add_argument('-p', '--port', help='port to listen on')
+    parser.add_argument('-i', '--interface', help='inteface to listen on')
 
     args = parser.parse_args()
 
-    server = SimpleXMLRPCServer(("localhost", int(args.port)))
+    server = SimpleXMLRPCServer((get_ip(args.interface if args.interface else 'eth0'), int(args.port if args.port else 8000)))
     server.register_function(Worker(args.unet_dir), "detect_bbox")
 
     try:
